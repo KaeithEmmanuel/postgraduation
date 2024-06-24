a3a61691f2138d5e5a86cb8823cde0fa2b6c6066
KaeithEmmanuel <kaeithemmanueleddu@gmail.com>
2024-06-24 22:44:56 +0530

    webp done

---
+++ higherstudies/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  // Other webpack configuration options
+  resolve: {
+    fallback: {
+      os: require.resolve("os-browserify/browser"),
+    }
+  }
+};
